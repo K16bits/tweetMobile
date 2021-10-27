21a2105a6ba1bf8525ad36cfa781ab75b823e65d
Cristiano <cristiano.16bits@gmail.com>
2021-10-27 16:09:09 -0300

    screen home e icon

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import {Text,View} from 'react-native'
 import { styled } from './styles'
+import { AntDesign } from '@expo/vector-icons';
+
 
 export default function HomeScreen(){
     return(
         <View style={styled.container}>
             <View style={styled.mainContainer}>
+                    <AntDesign
+                    name="message1" color="white" size={60}/>
                 <Text style={styled.text}>
                     Wellcome to tweet 
                 </Text>
--- src/screens/HomeScreen/styles.js
+++ src/screens/HomeScreen/styles.js
@@ -1,19 +1,25 @@
 import {StyleSheet} from 'react-native'
 import theme from '../../themes/theme'
+import {getStatusBarHeight} from 'react-native-iphone-x-helper'
+import {Dimensions} from 'react-native'
+
+const height = Dimensions.get('screen').height;
 
 export const styled = StyleSheet.create({
     container:{
         flex:1,
         backgroundColor:theme.colors.background,
+        marginTop: getStatusBarHeight()+5,
     },
     text:{
         color:'white',
         fontFamily: theme.fonts.font900,
         fontSize:30,
+        margin:10,
     },
     mainContainer:{
-        marginTop:50,
+        marginTop:height*0.2,
         justifyContent:'center',
         alignItems:'center'
-    }
+    },
 })                   
