c521afd133a6889cbce7e9d07f3260e7ef9b9aad
Cristiano <cristiano.16bits@gmail.com>
2021-11-03 19:04:22 -0300

    conexão sqlite3,migrations e  insert

--- backend/src/controllers/crud.js
+++ backend/src/controllers/crud.js
@@ -1,3 +1,5 @@
+const knex = require("../database/dbConfig");
+
 module.exports = {
     async store(req,res){
         const {name,password} = req.body;
@@ -5,6 +7,14 @@ module.exports = {
         if(!name || !password){
             return res.status(401).json({"status":"não foi passado nome e senha"})
         }
+        await knex('users').insert({
+            "email":name,
+            "password":password
+        }).then(()=>{
+            console.log("deu bom")
+        }).catch(erro =>{
+            console.log(erro);
+        })
         return res.status(200).json({"status":"deu bom"})
     }
 }
